95460c2675d685911bc299717851fee54909cf0d
Nick Wei <weijin84@gmail.com>
2019-01-17 00:19:19 -0800

    encode and decode bits for dns message

--- e2/main.go
+++ e2/main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"time"
 )
 
 const (
@@ -14,12 +13,14 @@ const (
 
 func main() {
 	var dnsAddr string
-	flag.StringVar(&dnsAddr, "addr", "8.8.8.8", "the address of the DNS server")
+	var resolveAddr string
+	flag.StringVar(&dnsAddr, "dns", "8.8.8.8", "the address of the DNS server")
+	flag.StringVar(&resolveAddr, "addr", "", "the address to resolve")
 	flag.Parse()
 
-	addr := fmt.Sprintf("%s:80", dnsAddr)
-	fmt.Printf("dailing %s...\n\n", addr)
-	conn, err := net.Dial("tcp", addr)
+	addr := fmt.Sprintf("%s:53", dnsAddr)
+	fmt.Printf("dailing %s\n\n", addr)
+	conn, err := net.Dial("udp", addr)
 	handleErr(err)
 
 	defer func() {
@@ -27,21 +28,17 @@ func main() {
 		handleErr(closeErr)
 	}()
 
-	req := []byte("HEAD / HTTP/1.1\r\n\r\n")
+	req := encodeRequest(resolveAddr)
 	m, err := conn.Write(req)
 	handleErr(err)
 	fmt.Printf("wrote %d bytes\n", m)
 	fmt.Printf("%s\n", req)
 
-	res := make([]byte, kbSize*kbSize)
-	for {
-		n, readErr := conn.Read(res)
-		handleErr(readErr)
-		fmt.Printf("read %d bytes\n", n)
-		fmt.Printf("%s\n", res)
-
-		time.Sleep(time.Second)
-	}
+	res := make([]byte, kbSize)
+	n, err := conn.Read(res)
+	handleErr(err)
+	fmt.Printf("read %d bytes\n", n)
+	decodeResponse(res)
 }
 
 func handleErr(err error) {
@@ -49,4 +46,39 @@ func handleErr(err error) {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+}
+
+// good blog about bit manipulation in go: https://ops.tips/blog/raw-dns-resolver-in-go/
+func encodeRequest(addr string) []byte {
+	req := make([]byte, 0, 12)
+	req = append(req, []byte{0,1}...) // ID = hardcoded
+	// req = append(req, uint(0)) // QR = query
+	// req = append(req, []uint{0,0,0,0}...) // Opcode = standard query
+	// req = append(req, uint(0)) // AA (server side)
+	// req = append(req, uint(0)) // TC = not truncated
+	// req = append(req, uint(1)) // RD = recursion is desired
+	req = append(req, 1)
+	// req = append(req, uint(0)) // RA (server side)
+	// req = append(req, []uint{0,0,0}...) // Z (server side)
+	// req = append(req, []uint{0,0,0,0}...) // Rcode (server side)
+	req = append(req, 0)
+	// req = append(req, []uint{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1}...) // QDCount = 1 question
+	req = append(req, []byte{0,1}...)
+	// req = append(req, []uint{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}...) // ANCount (server side)
+	// req = append(req, []uint{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}...) // NSCount (server side)
+	// req = append(req, []uint{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}...) // ARCount (server side)
+	req = append(req, []byte{0,0,0,0,0,0}...)
+	return req
+}
+
+func decodeResponse(res []byte) {
+	fmt.Println("headers")
+	fmt.Printf("id: %s\n", res[0:2])
+	fmt.Printf("options: %s\n", res[2:3])
+	fmt.Printf("server options: %s\n", res[3:4])
+	fmt.Printf("question count: %s\n", res[4:6])
+	fmt.Printf("answer count: %s\n", res[6:8])
+	fmt.Printf("ns count: %s\n", res[8:10])
+	fmt.Printf("ar count: %s\n", res[10:12])
+	fmt.Println("message")
 }
